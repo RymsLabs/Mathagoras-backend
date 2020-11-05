d0d7b83f3719ff85ade47c4df4466e5c281cdd17
dramikei <raghav2011rv@gmail.com>
2020-11-05 18:10:11 +0530

    implemented messages controller

--- Controller/messages-controller.js
+++ Controller/messages-controller.js
@@ -2,11 +2,62 @@ const DiscussionMessages = require('../Model/discussion-messages');
 const {validationResult} = require("express-validator");
 
 const getAll = (req, res) => {
+    const messages = await DiscussionMessages.findAll();
+    res.json({
+        "type": "success",
+        "messages": messages
+    });
+}
 
+const addMessage = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400);
+        return res.json({
+            'type': 'error',
+            'message': 'Incorrect parameters.'
+        });
+    }
+
+    const { discussionId, message } = req.body;
+
+    let userId, userType;
+
+    if (req.user.student_id != null) {
+        userId = req.user.student_id;
+        userType = "student";
+    } else {
+        userId = req.user.teacher_id;
+        userType = "teacher";
+    }
+
+    let message;
+    try {
+        message = await DiscussionMessages.create({
+            discussion_id: discussionId,
+            user_id: userId,
+            user_type: userType,
+            message_time: Date.now(),
+            message,
+
+        });
+    } catch (err) {
+        res.status(500);
+        return res.json({
+            "type": "error",
+            "message": "Error sending message.",
+            "err": err
+        });
+    }
+
+    res.json({
+        "type":"success",
+        "class": message
+    });
 }
 
 
 exports.getAll = getAll
-// exports.addDiscussion = addDiscussion;
+exports.addMessage = addMessage;
 // exports.updateDiscussion = updateDiscussion;
 // exports.deleteDiscussion = deleteDiscussion;
