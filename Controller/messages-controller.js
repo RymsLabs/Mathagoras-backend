00cc22eb44c1073adec157c3dcac66b0587e33c1
dramikei <raghav2011rv@gmail.com>
2020-11-18 01:33:02 +0530

    Add userName in messages

--- Controller/messages-controller.js
+++ Controller/messages-controller.js
@@ -1,11 +1,58 @@
 const DiscussionMessages = require('../Model/discussion-messages');
 const {validationResult} = require("express-validator");
+const Student = require('../Model/student');
+const Teacher = require('../Model/teacher');
 
 const getAll = async (req, res) => {
     const { discussionId } = req.params;
     const messages = await DiscussionMessages.findAll({
         where: {
             discussion_id: discussionId,
+        }
+    });
+    messages = messages.map(async (message) => {
+        if(message.user_type == 'student') {
+            let Sname, student;
+            try {
+                student = await Student.findOne({where: {student_id: message.user_id}});
+            } catch (err) {
+                res.status(500);
+                return res.json({
+                    "type":"error",
+                    "message":"Error quering Students",
+                    "err":err
+                });
+            }
+            if (student.lname) {
+                Sname = `${student.fname} ${student.lname}`;
+            } else {
+                Sname = student.fname;
+            }
+
+            return Object.defineProperty(message, {
+                username: Sname
+            });
+        } else {
+            let Tname, teacher;
+            try {
+                teacher = await Teacher.findOne({where: {teacher_id: message.user_id}});
+            } catch (err) {
+                res.status(500);
+                return res.json({
+                    "type":"error",
+                    "message":"Error quering Teachers",
+                    "err":err
+                });
+            }
+            if (teacher.lname) {
+                Tname = `${teacher.fname} ${teacher.lname}`;
+            } else {
+                Tname = teacher.fname;
+            }
+
+            return Object.defineProperty(message, {
+                username: Tname
+            });
         }
     });
     res.json({
@@ -62,7 +109,7 @@ const addMessage = async (req, res) => {
 }
 
 
-exports.getAll = getAll
+exports.getAll = getAll;
 exports.addMessage = addMessage;
 // exports.updateDiscussion = updateDiscussion;
 // exports.deleteDiscussion = deleteDiscussion;
